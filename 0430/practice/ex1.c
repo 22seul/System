7725f8ce04e754247659889752c280e68795d879
22seul <143988446+22seul@users.noreply.github.com>
2024-04-30 22:36:45 +0900

    Add files via upload

---
+++ 0430/practice/ex1.c
@@ -0,0 +1,7 @@
+//stat()
+#include <sys/types.h>
+#include <sys/stat.h>
+int stat (const char *filename, struct stat *buf);
+int fstat (int fd, struct stat *buf);
+int lstat (const char *filename, struct stat *buf);
+
